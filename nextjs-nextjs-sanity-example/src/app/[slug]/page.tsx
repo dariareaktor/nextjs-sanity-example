283ab92fe1801a267b687ea2bf401465e35a53cb
Daria Artemova <daria.artemova@reaktor.com>
2024-11-29 15:18:23 +0100

    fix next build

--- nextjs-nextjs-sanity-example/src/app/[slug]/page.tsx
+++ nextjs-nextjs-sanity-example/src/app/[slug]/page.tsx
@@ -1,16 +1,16 @@
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+// import imageUrlBuilder from "@sanity/image-url";
+// import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import { client } from "@/sanity/client";
 import { SanityDocument } from "next-sanity";
 
 const PAGE_QUERY = `*[_type == "page" && slug.current == $slug][0]{ _id, title, slug, pageBuilder }`;
 
-const { projectId, dataset } = client.config();
-const urlFor = (source: SanityImageSource) => {
-  return projectId && dataset
-    ? imageUrlBuilder({ projectId, dataset }).image(source)
-    : null;
-};
+// const { projectId, dataset } = client.config();
+// const urlFor = (source: SanityImageSource) => {
+//   return projectId && dataset
+//     ? imageUrlBuilder({ projectId, dataset }).image(source)
+//     : null;
+// };
 
 const options = { next: { revalidate: 30 } };
 
